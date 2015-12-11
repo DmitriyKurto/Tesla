16b8a70c0ea92157600fce976b7cbf8bc8d42bda
Dmitriy Kurto <dmitrykurto@gmail.com>
2015-12-11 21:45:40 +0300

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,26 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     del = require('del'),
     connect = require('gulp-connect'),
-    usemin = require('gulp-usemin');
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin');
+
+var config = {
+    htmlMain: [
+        'app/index.html'
+    ],
+    html: [
+        'app/pages/**/*.html'
+    ],
+    css:[
+        'app/css/**/*.css'
+    ],
+    img: [
+        'app/img/**/*.{png,jpg,jpeg,gif,svg}'
+    ],
+    scripts:[
+        'app/js/**/*.js'
+    ]
+};
 
 gulp.task('default', ['public', 'webserver', 'watch']);
 gulp.task('public', function(callback) {
@@ -22,33 +41,34 @@ gulp.task('webserver', function() {
     });
 });
 gulp.task('watch', function(){
-    gulp.watch('app/*.html', ['usemin']);
-    gulp.watch('app/pages/*.html', ['html']);
-    gulp.watch('app/css/*.css', ['usemin']);
-    gulp.watch('app/js/**/*.js', ['usemin']);
-    gulp.watch('app/img/**/*.*', ['img']);
+    gulp.watch(config.htmlMain, ['usemin']);
+    gulp.watch(config.html, ['html']);
+    gulp.watch(config.css, ['usemin']);
+    gulp.watch(config.scripts, ['usemin']);
+    gulp.watch(config.img, ['img']);
 });
 gulp.task('del', function () {
     return del('dist/');
 });
 gulp.task('html-index', function(){
-    gulp.src('app/*.html')
+    gulp.src(config.htmlMain)
         //.pipe(minHTML())
         .pipe(gulp.dest('dist/'));
 });
 gulp.task('html', function(){
-   gulp.src('app/pages/*.html')
+   gulp.src(config.html)
         .pipe(minHTML())
         .pipe(gulp.dest('dist/pages/'))
         .pipe(connect.reload());
 });
 gulp.task('img', function(){
-    gulp.src('app/img/**/*.*')
+    gulp.src(config.img)
+        .pipe(imagemin())
         .pipe(gulp.dest('dist/img'))
         .pipe(connect.reload());
 });
 gulp.task('usemin', ['html-index'], function() {
-    return gulp.src('app/index.html')
+    return gulp.src(config.htmlMain)
         .pipe(usemin({
             css: [minCSS(), 'concat'],
             js: ['concat'],
